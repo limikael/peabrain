e17417884052ffa60c46ef3ec30e52b4ab871356
Mikael Lindqvist <li.mikael@gmail.com>
2026-05-02 22:13:59 +0800

    example

--- examples/basic/test.js
+++ examples/basic/test.js
@@ -1,8 +1,13 @@
+let count=0;
+
 function update() {
-	Lcd.getInstance().setBuffer("Val: "+getUiKnob().getValue());
+	Lcd.getInstance().setBuffer("Val: "+getUiKnob().getValue()+" Count: "+count);
 }
 
 getUiKnob().on("change",update);
 update();
 
-getUiButton().on("down",v=>console.log("press..."));
+getUiButton().on("down",v=>{
+	count++;
+	update();
+});
