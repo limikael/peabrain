c190c763e3397a14afde137203a983a358465c5d
Mikael Lindqvist <li.mikael@gmail.com>
2026-06-27 20:46:28 +0800

    export

--- packages/peabrain-ui/js/exports-mcu.js
+++ packages/peabrain-ui/js/exports-mcu.js
@@ -1,3 +1,3 @@
-export {renderController, useEncoderButton} from "./device-ui.js";
+export {renderController, useEncoderButton, useEncoderDelta} from "./device-ui.js";
 export {Menu, MenuItem, useBack, useEventUpdate, StatusCover, useIsBootComplete} from "./components.jsx";
 export {ObjectEditor, Job} from "./can-ui.jsx";
